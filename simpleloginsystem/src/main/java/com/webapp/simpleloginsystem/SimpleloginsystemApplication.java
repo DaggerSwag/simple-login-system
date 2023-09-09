4844df351c697d08ffe9598b3193c53907ce5699
Yashwanth Kumar <yashwanthkumarofficial@gmail.com>
2023-09-09 22:31:17 +0530

    first_commit

---
+++ simpleloginsystem/src/main/java/com/webapp/simpleloginsystem/SimpleloginsystemApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.simpleloginsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleloginsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleloginsystemApplication.class, args);
+	}
+
+}
---
+++ simpleloginsystem/src/test/java/com/webapp/simpleloginsystem/SimpleloginsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.simpleloginsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleloginsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
